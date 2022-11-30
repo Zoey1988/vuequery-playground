e2e62ec0146c0d408f0c2302b40018f47c9d1c7b
Zeinab Rezaee <zeinab.rezaee67@gmail.com>
2022-11-30 09:21:12 +0330

    Add todo-1

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -5,5 +5,13 @@ export const api = axios.create({
   timeout: 10000,
 });
 
-api.defaults.headers.common["Authorization"] =
-  "Bearer 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";
+// TODO-1 Change access token please :)
+
+// To get your access token, follow this instruction
+// https://docs.github.com/en/enterprise-cloud@latest/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token#creating-a-personal-access-token-classic
+
+// If you are fartoot and lazy like Iman or you couldn't get access token for any reason, use mine with cautious
+
+const ACCESS_TOKEN = "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";
+
+api.defaults.headers.common["Authorization"] = `Bearer ${ACCESS_TOKEN}`;
